e3cd7ca4beee48dccad08c09f8f2f4efd86c5302
AbayaJohnCarloE <146050272+AbayaJohnCarloE@users.noreply.github.com>
2024-04-27 16:02:54 +0800

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var logoutBtn = document.getElementById("logoutBtn");
+    
+    logoutBtn.addEventListener("click", function() {
+    
+      window.location.href = "index.html";
+    });
+  });
+
+  
+  
